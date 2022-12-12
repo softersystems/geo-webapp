5846d3a1b28073513fdb7a9ff13aadb18cd802f0
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2022-12-12 15:50:50 +0100

    move landing page to about, add title to new landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,61 +10,14 @@ const fontDigitalDream = localFont({ src: "../public/Digitaldream.ttf" });
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-20 text-layers-pink font-extralight bg-dark-brown-grey flex flex-col justify-around items-center">
-      <div className="max-w-md px-20 ">
-        <Image src={planetBoi} alt="Picture of the author" className="fit" />
-      </div>
-      <h2 className="text-center mt-6 ">
-        A&nbsp;&nbsp;&nbsp;
-        <Link href="https://girly.network" target="_blank">
-          <span className={`leading-[3rem] ${fontDigitalDream.className}`}>
-            GIRLY
-          </span>
-        </Link>
-        &nbsp;&nbsp;project.
-        <p />
-        Now in development.
-      </h2>
-      <div className="flex justify-center items-center gap-8">
-        <Link href="https://twitter.com/layerslive" target="_blank">
-          <Image
-            src={twitterIcon}
-            alt="Twitter: updates about the project."
-            height={30}
-            width={30}
-          />
-        </Link>
-        <Link
-          href="https://app.commonground.cg/community/NCTwbWjKE5?ref=hpYWNFp7"
-          target="_blank"
-        >
-          <Image
-            src={commonGroundIcon}
-            alt="Common Ground: team and community chat server."
-            height={32}
-            width={32}
-          />
-        </Link>
-        <Link href="https://github.com/GIRLYDAO/layers-webapp" target="_blank">
-          <Image
-            src={gitHubIcon}
-            alt="GitHub: public codebase."
-            height={30}
-            width={30}
-          />
-        </Link>
-
-        {/* <div className="w-2/3 shadow-md p-6 rounded-lg mb-8 flex flex-col items-center">
-          <p className="text-base text-gray-400  text-center mt-2"></p>
-          <Link href={`/profile/`}>
-            <p className="cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2">
-              Link{" "}
-            </p>
-          </Link>
-          <p className="text-pink-600 text-sm font-medium text-center">
-            followers
-          </p>
-        </div> */}
+    <div className="min-h-screen text-layers-pink font-extralight bg-dark-brown-grey">
+      <div className="fixed flex flex-row justify-around items-center">
+        <h1 className="text-center p-6">
+          <Link href="/">Layers</Link>
+        </h1>
+        <h2>
+          <Link href="/about">(About)</Link>
+        </h2>
       </div>
     </div>
   );
