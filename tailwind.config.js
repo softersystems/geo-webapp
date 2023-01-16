79b558d16c617ef94e62a8bee38d17554636ef10
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-01-16 01:08:46 +0100

    add text about layers and fix social icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ module.exports = {
     extend: {
       colors: {
         "dark-brown-grey": "#221c1c",
+        "lighter-brown-grey": "#2B2324",
         "layers-pink": "#F2ACB3",
         "layers-pink-light": "#FDC6CC",
+      },
+      backgroundImage: {
+        "layers-gradient":
+          "radial-gradient(43.57% 47.2% at 96.26% 87.08%, rgb(97 0 255 / 8%) 6.42%, rgba(255, 255, 255, 0.2) 82.47%), linear-gradient(191.87deg, #f5aabc 9.02%, #87FDD2 152.67%)",
       },
     },
   },
